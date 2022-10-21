3181b2fa730fab5918bb8eacb18936f914515899
Cheryl Mellor <cherylmmellor@gmail.com>
2022-10-21 16:20:42 -0700

    Removed text from headings

--- src/CompletedProjectsAccordion.js
+++ src/CompletedProjectsAccordion.js
@@ -13,25 +13,24 @@ import PhotoCards from "./PhotoCards";
 const CompletedProjectsAccordion = () => {
 
     return (
-
     <Accordion>
         <AccordionItem>
             <AccordionItemHeading>
-                <AccordionItemButton><h2 className='accordion_section_heading' id='Amigurumi_section'>Amigurumi - Click to expoand</h2></AccordionItemButton>
+                <AccordionItemButton><h2 className='accordion_section_heading' id='Amigurumi_section'>Amigurumi</h2></AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel>
                 <PhotoCards section='Amigurumi' /></AccordionItemPanel>
         </AccordionItem>
         <AccordionItem>
             <AccordionItemHeading>
-                <AccordionItemButton><h2 className='accordion_section_heading' id='Crochet_section'>Crochet projects - click to expand</h2></AccordionItemButton>
+                <AccordionItemButton><h2 className='accordion_section_heading' id='Crochet_section'>Crochet projects</h2></AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel>
                 <PhotoCards section='Crochet' /></AccordionItemPanel>
         </AccordionItem>
         <AccordionItem>
         <AccordionItemHeading>
-            <AccordionItemButton><h2 className='accordion_section_heading' id='Quilting_section'>Quilting projects - click to expand</h2></AccordionItemButton>
+            <AccordionItemButton><h2 className='accordion_section_heading' id='Quilting_section'>Quilting projects</h2></AccordionItemButton>
         </AccordionItemHeading>
         <AccordionItemPanel>
             <PhotoCards section='Quilts' /></AccordionItemPanel>
