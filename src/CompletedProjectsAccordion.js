363b16d2e352e39d3aeba3d3e674cf72b13d4a2f
Cheryl Mellor <cherylmmellor@gmail.com>
2022-10-26 17:23:58 -0700

    Removed unused 'accordionItemHeading'

--- src/CompletedProjectsAccordion.js
+++ src/CompletedProjectsAccordion.js
@@ -3,7 +3,6 @@ import React from 'react';
 import {
     Accordion,
     AccordionItem,
-    AccordionItemHeading,
     AccordionItemButton,
     AccordionItemPanel,
 } from 'react-accessible-accordion';
@@ -15,23 +14,17 @@ const CompletedProjectsAccordion = () => {
     return (
     <Accordion allowMultipleExpanded allowZeroExpanded>
         <AccordionItem>
-            <AccordionItemHeading>
-                <AccordionItemButton><h2 className='accordion_section_heading' id='Amigurumi_section'>Amigurumi</h2></AccordionItemButton>
-            </AccordionItemHeading>
+                <AccordionItemButton><h2 id='Amigurumi_section'>Amigurumi</h2><div className={'arrow'}/></AccordionItemButton>
             <AccordionItemPanel>
                 <PhotoCards section='Amigurumi' /></AccordionItemPanel>
         </AccordionItem>
         <AccordionItem>
-            <AccordionItemHeading>
-                <AccordionItemButton><h2 className='accordion_section_heading' id='Crochet_section'>Crochet projects</h2></AccordionItemButton>
-            </AccordionItemHeading>
+                <AccordionItemButton><h2 id='Crochet_section'>Crochet projects</h2><div className={'arrow'}/></AccordionItemButton>
             <AccordionItemPanel>
                 <PhotoCards section='Crochet' /></AccordionItemPanel>
         </AccordionItem>
         <AccordionItem>
-        <AccordionItemHeading>
-            <AccordionItemButton><h2 className='accordion_section_heading' id='Quilting_section'>Quilting projects</h2></AccordionItemButton>
-        </AccordionItemHeading>
+            <AccordionItemButton><h2 id='Quilting_section'>Quilting projects</h2><div className={'arrow'}/></AccordionItemButton>
         <AccordionItemPanel>
             <PhotoCards section='Quilts' /></AccordionItemPanel>
     </AccordionItem>
