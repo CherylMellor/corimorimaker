307d1aac917e4d3aa97dca2577696c155c392bb0
Cheryl Mellor <cherylmmellor@gmail.com>
2023-01-20 17:30:29 -0800

    Changed the width of the border

--- src/PatternLinks.js
+++ src/PatternLinks.js
@@ -6,7 +6,7 @@ const PatternLinks = (props) => {
         <div id='Pattern_Links'>
 
             <h2 style={{borderTop: 'solid 2px #708c82',
-                paddingTop: '15px', textAlign: 'center', color: '#275263'
+                paddingTop: '15px', textAlign: 'center', color: '#275263', width: '80%', margin: '0 auto',
             }}>Links to the patterns used can be found here:</h2>
             <SearchBar section={section} />
             </div>
