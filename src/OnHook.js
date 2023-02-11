60ee21aec67e3e749bef82e798e0960566e9cccb
Cheryl Mellor <cherylmmellor@gmail.com>
2023-02-10 18:07:01 -0800

    Changed the order of the picture and text

--- src/OnHook.js
+++ src/OnHook.js
@@ -9,19 +9,19 @@ const OnHook = () => {
             <div style={{
                 position: 'relative',
                 display: 'grid',
-                gridTemplateColumns: '1fr 1fr',
+                gridTemplateColumns: '0.75fr 1.25fr',
+                paddingTop: '10px',
             }}>
-                <div style={{gridColumn: 1,}}>
+                <div style={{gridColumn: 1, alignSelf: "flex-start",
+                    textAlign: "center",}}>
+                    <img src='/Photos/Crochet/Schnauser.jpg' alt='Crochet Schnauser'/>
+                </div>
+                <div style={{gridColumn: 2,}}>
             <h2>Crochet Schnauser</h2>
                 <br></br>
             <p>I thought this was so cute and had to give it a go.</p>
                 <p>This will be a thank you gift for a #BikeMS donation</p>
             <p>The free pattern I found on the <a href="https://www.projectarian.com/" >Projectarian</a> website here: <a href="https://www.projectarian.com/2017/01/09/amigurumi-dog-fur-free-crochet/">Schnauser Pattern</a></p>
-            </div>
-
-            <div style={{gridColumn: 2, alignSelf: "center",
-                textAlign: "center",}}>
-                <img src='/Photos/Crochet/Schnauser.jpg' alt='Crochet Schnauser'/>
             </div>
             </div>
         </div>
@@ -32,21 +32,21 @@ const OnHook = () => {
         <div style={{
             position: 'relative',
             display: 'grid',
-            gridTemplateColumns: '1fr 1fr',
+            gridTemplateColumns: '0.75fr 1.25fr',
+            paddingTop: '10px',
         }}>
-            <div style={{gridColumn: 1,}}>
+            <div style={{gridColumn: 1,
+                alignSelf: "flex-start",
+                textAlign: "center",
+            }}>
+                <img src='/Photos/Crochet/Blob monster blue with yellow horns.jpg' alt='Crochet Blob Monster' />
+            </div> <div style={{gridColumn: 2,}}>
                 <h2>Blob Monster</h2>
                 <br></br>
             <p>A commission for a Blob Monster in team colors: Blue and Yellow </p>
             <p>This will be a thank you gift for a #BikeMS donation</p>
             <p>The pattern made by Megan Lapp from <a href="https://craftyintentions.com/">Crafty Intentions</a></p> <p>Purchased from here: <a href="https://www.ravelry.com/patterns/library/blob-monster/">Blob Monster</a></p>
     </div>
-            <div style={{gridColumn: 2,
-            alignSelf: "center",
-            textAlign: "center",
-            }}>
-        <img src='/Photos/Crochet/Blob monster blue with yellow horns.jpg' alt='Crochet Blob Monster' />
-            </div>
             </div>
         </div>
 </div>
