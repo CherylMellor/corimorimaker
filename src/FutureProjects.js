0a95eb4f7c2ca060a4023835d4e63a9ab5ce9230
Cheryl Mellor <cherylmmellor@gmail.com>
2023-02-10 18:24:52 -0800

    Changed h3 to a h2

--- src/FutureProjects.js
+++ src/FutureProjects.js
@@ -3,7 +3,7 @@ const FutureProjects = () => {
     return (
         <div className='future-projects'>
            <h1 style={{textAlign: 'center', paddingBottom: '5px',}}>Future Projects</h1>
-               <h3 style={{textAlign: 'center',}}>Here are a few things that I'm considering for my next project</h3>
+               <h2 style={{textAlign: 'center',}}>Here are a few things that I'm considering for my next project</h2>
            <ul>
                <li>Robots - crochet pattern already purchased from Crafty Intentions</li>
                <li>Flying Saucer - I haven't got the pattern yet but I like this one - Crochet pattern lalylala UFO</li>
