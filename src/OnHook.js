2b528fcc1e6f4d0fc056796befb678797771267a
Cheryl Mellor <cherylmmellor@gmail.com>
2023-02-10 18:30:48 -0800

    Centered the titles, removed the br, added padding to the bottom of the h2

--- src/OnHook.js
+++ src/OnHook.js
@@ -4,7 +4,7 @@ const OnHook = () => {
     return (
         <div className='current-section'>
         <div className='current-on-hook'>
-            <h1>Currently On-Hook</h1>
+            <h1 style={{textAlign: 'center',}}>Currently On-Hook</h1>
 
             <div style={{
                 position: 'relative',
@@ -17,8 +17,7 @@ const OnHook = () => {
                     <img src='/Photos/Crochet/Schnauser.jpg' alt='Crochet Schnauser'/>
                 </div>
                 <div style={{gridColumn: 2,}}>
-            <h2>Crochet Schnauser</h2>
-                <br></br>
+            <h2 style={{paddingBottom: '10px'}}>Crochet Schnauser</h2>
             <p>I thought this was so cute and had to give it a go.</p>
                 <p>This will be a thank you gift for a #BikeMS donation</p>
             <p>The free pattern I found on the <a href="https://www.projectarian.com/" >Projectarian</a> website here: <a href="https://www.projectarian.com/2017/01/09/amigurumi-dog-fur-free-crochet/">Schnauser Pattern</a></p>
@@ -27,7 +26,7 @@ const OnHook = () => {
         </div>
 
     <div className='current-on-hook'>
-        <h1>Recently Completed</h1>
+        <h1 style={{textAlign: 'center',}}>Recently Completed</h1>
 
         <div style={{
             position: 'relative',
@@ -41,8 +40,7 @@ const OnHook = () => {
             }}>
                 <img src='/Photos/Crochet/Blob monster blue with yellow horns.jpg' alt='Crochet Blob Monster' />
             </div> <div style={{gridColumn: 2,}}>
-                <h2>Blob Monster</h2>
-                <br></br>
+                <h2  style={{paddingBottom: '10px'}}>Blob Monster</h2>
             <p>A commission for a Blob Monster in team colors: Blue and Yellow </p>
             <p>This will be a thank you gift for a #BikeMS donation</p>
             <p>The pattern made by Megan Lapp from <a href="https://craftyintentions.com/">Crafty Intentions</a></p> <p>Purchased from here: <a href="https://www.ravelry.com/patterns/library/blob-monster/">Blob Monster</a></p>
