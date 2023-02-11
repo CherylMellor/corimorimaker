3804f48b3ea7f5204f170763f8f28ae01faece2f
Cheryl Mellor <cherylmmellor@gmail.com>
2023-02-10 19:44:49 -0800

    Removed phone number place-holder and added fundraising info

--- src/Footer.js
+++ src/Footer.js
@@ -2,10 +2,11 @@
 const Footer = () => {
     return (
         <div className={"footer"}>
-            <h2>Contact info</h2>
-            <p>Cheryl Mellor</p>
-            <p>Email address:...</p>
-            <p>Phone Number: ...</p>
+            <h2>Contact info:</h2>
+            <p>Cheryl Mellor e-mail address:  </p>
+            <h2>To date my BikeMS:Waves to Wine fundraising stands at $7,234.91</h2>
+            <p>Donations to my BikeMS:Waves to Wine 2023 fundraiser can be made here: https://mssociety.donordrive.com/participant/CherylMellor</p>
+            <p style={{textAlign: 'right', fontSize: '14px' }}>Website updated: 2/10/2023</p>
         </div>
     )
 }
