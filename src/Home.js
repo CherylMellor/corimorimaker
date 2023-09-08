4aaa090351daa76d140ac51f8b324e1be3897f49
Cheryl Mellor <cherylmmellor@gmail.com>
2023-09-08 16:47:06 -0700

    Changed some wording and corrected spelling Added background color to some boxes Added new images

--- src/Home.js
+++ src/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
     <div style={{display: 'flex', flexDirection: 'column',}}>
         <p>I started my crafting adventure after being diagnosed with Multiple Sclerosis.</p>
         <p> My manual dexterity was waining and I wanted a way to maintain what I had and regain what I'd lost, so I started hand crafting. </p>
-        <p>I had a sewing kit and bought some fabric and hand sowed a bespoke baby blanket for my friends first child. Along with regaining my ability to complete small precise movements, I gained a love for seeing something come together from its component parts. Hand sewing however was not for me, instead I bought a sowing machine and quilting is so much quicker but still fun. This presented me with a dilemma, what to do to maintain my manual dexterity?  This brought me to crochet, turns out I like that too, now I have 2 hobbies.</p>
+        <p>I had a sewing kit and bought some fabric and handmade a bespoke baby blanket for my friends first child. Along with regaining my ability to complete small precise movements, I gained a love for seeing something come together from its component parts. Hand sewing however was not for me, instead I bought a sewing machine and quilting is now so much quicker but still fun. This presented me with a dilemma, what to do to maintain my manual dexterity?  This brought me to crochet, turns out I like that too, now I have 2 hobbies.</p>
         <p>I love to make items to gift to friends just to spread some joy. But I also make items as a thank you gifts for donations to my #BikeMS fundraising.</p>
         <p>Scroll down to see what I'm currently working on and what I've completed</p>
 </div>
