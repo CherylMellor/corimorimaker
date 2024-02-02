d8f851b5c738dbf5d72ee2d5ed0709025e85eb02
Cheryl Mellor <cherylmmellor@gmail.com>
2024-02-01 17:27:57 -0800

    Removed unused div from Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,11 @@
 const Navbar = () => {
     return (
         <nav className={"Navbar"}>
-            <div className="links"></div>
             <a href="/">Home</a>
             <a href='#Amigurumi_section'>Amigurumi</a>
             <a href="#Crochet_section">Crochet</a>
             <a href='#Quilting_section'>Quilts</a>
+            <a href='#Embroidery_section'>Embroidery</a>
             <a href='#Pattern_Links'>Pattern Links</a>
         </nav>
     );
